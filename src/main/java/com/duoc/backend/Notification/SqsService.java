eebe186689885b96607b23546173230e27d7d9dc
agtttttttttttt <agus.valdes@duocuc.cl>
2026-07-10 16:49:36 -0400

    feat: configure AWS SQS with static credentials from properties
    - SqsService now uses AwsSessionCredentials from application.properties
    - Exclude real credentials from git (application.properties in .gitignore)
    - Add application.properties.example as template

--- src/main/java/com/duoc/backend/Notification/SqsService.java
+++ src/main/java/com/duoc/backend/Notification/SqsService.java
@@ -3,7 +3,9 @@ package com.duoc.backend.Notification;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.AwsSessionCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.sqs.SqsClient;
 import software.amazon.awssdk.services.sqs.model.SendMessageRequest;
@@ -17,10 +19,15 @@ public class SqsService {
 
     public SqsService(
             @Value("${aws.region:us-east-1}") String region,
+            @Value("${aws.accessKeyId}") String accessKeyId,
+            @Value("${aws.secretAccessKey}") String secretAccessKey,
+            @Value("${aws.sessionToken}") String sessionToken,
             @Value("${aws.sqs.queue.url}") String queueUrl) {
+        AwsSessionCredentials credentials = AwsSessionCredentials.create(
+                accessKeyId, secretAccessKey, sessionToken);
         this.sqsClient = SqsClient.builder()
                 .region(Region.of(region))
-                .credentialsProvider(DefaultCredentialsProvider.create())
+                .credentialsProvider(StaticCredentialsProvider.create(credentials))
                 .build();
         this.queueUrl = queueUrl;
     }
